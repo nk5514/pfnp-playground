3851d230ab84435eb737c54fa1f33e3ba00b352a
nk5514 <neerajkothari23@gmail.com>
2015-11-30 21:05:49 +0800

    Added Express Package
    Sure to replace email

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express()
+
+var message = 'How are you today?'
+
+app.get('/', function (req, res) {
+  res.send(message)
+})
+
+
+
+app.get('/edit/:motd', function (req, res) {
+  message=req.params.motd
+  res.send('Message Updated')
+})
+
+app.get('/you', function (req, res) {
+  res.send('You are the Man')
+})
+
+app.listen(3000)
